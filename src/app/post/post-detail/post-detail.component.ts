8a1492e2ffb7d31a6c0c3f1ff39c66134575e053
fuzhutech <fuzhutech@163.com>
2017-05-19 15:57:13 +0800

    修改页面title逻辑

--- src/app/post/post-detail/post-detail.component.ts
+++ src/app/post/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 import {ActivatedRoute, Router, Params} from '@angular/router';
 
 import {Post} from '../../shared/model/post-model';
@@ -13,7 +14,9 @@ export class PostDetailComponent implements OnInit {
   public post: Post = new Post();
 
   constructor(public postService: PostService,
-              public activeRoute: ActivatedRoute) {
+              public activeRoute: ActivatedRoute,
+              private titleService: Title) {
+    //
   }
 
   ngOnInit() {
@@ -22,10 +25,15 @@ export class PostDetailComponent implements OnInit {
     );
   }
 
+
   public getPost(id: number) {
     this.postService.getItem(id)
       .subscribe(
-        data => this.post = data,
+        data => {
+          this.post = data;
+          console.log(this.titleService.getTitle());
+          this.titleService.setTitle(this.post.title);
+        },
         error => console.error(error)
       );
   }
