a326b025e20aa9e80165823ef1722d9944026b8b
fuzhutech <fuzhutech@163.com>
2018-01-18 23:42:04 +0800

    改造angular-material：post-table组件

--- src/app/manage/post-table/post-table.component.ts
+++ src/app/manage/post-table/post-table.component.ts
@@ -1,97 +1,88 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, AfterViewInit} from '@angular/core';
 import {flyIn} from '../../shared/animations/fly-in';
 import {ActivatedRoute, Router, UrlTree, PRIMARY_OUTLET, UrlSegmentGroup, UrlSegment} from '@angular/router';
 import {PostService} from '../../shared';
 import {Post} from '../../shared/model/post-model';
 
+import {MatTableDataSource, MatPaginator, MatSort} from '@angular/material';
+import {ViewChild} from '@angular/core';
+
 @Component({
-  selector: 'app-post-table',
-  templateUrl: './post-table.component.html',
-  styleUrls: ['./post-table.component.scss'],
-  animations: [
-    flyIn
-  ]
+    selector: 'app-post-table',
+    templateUrl: './post-table.component.html',
+    styleUrls: ['./post-table.component.scss'],
+    animations: [
+        flyIn
+    ]
 })
-export class PostTableComponent implements OnInit {
-  @Input() dataURL = 'src/mock-data/post-list-mock.json';
+export class PostTableComponent implements OnInit, AfterViewInit {
+    displayedColumns = ['id', 'title', 'userName', 'customDate', 'select'];
+    dataSource = new MatTableDataSource<Post>();
 
-  public postList: Array<Post>;
-  public maxSize = 5;
-  public itemsPerPage = 5;
-  public totalItems = 15;
-  public currentPage = 1;
-  public numPages;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
 
-  constructor(public router: Router,
-              public activeRoute: ActivatedRoute,
-              public postService: PostService) {
-  }
+    constructor(public router: Router,
+                public activeRoute: ActivatedRoute,
+                public postService: PostService) {
+    }
 
-  ngOnInit() {
-    const offset = (this.currentPage - 1) * this.itemsPerPage;
-    // const end = (this.currentPage) * this.itemsPerPage;
-    const rows = this.itemsPerPage;
-    const total = -1;
-
-    this.loadData(offset, rows, total);
-  }
-
-  public pageChanged(event: any): void {
-    /*const urlTree: UrlTree = this.router.parseUrl(this.router.url);
-     const g: UrlSegmentGroup = urlTree.root.children[PRIMARY_OUTLET];
-     const s: UrlSegment[] = g.segments;
-     this.router.navigateByUrl(s[0] + '/posttable/page/' + event.page);*/
+    ngOnInit() {
+        this.loadData();
+    }
 
 
-    const offset = (event.page - 1) * event.itemsPerPage;
-    const rows = event.itemsPerPage;
+    public loadData() {
 
-    this.loadData(offset, rows, -1);
-  }
-
-  public loadData(offset: number, rows: number, total: number) {
-
-    this.postService.getListByPageInfo(offset, rows, -1).subscribe(
-      (data: { rows, total }) => {
-        this.postList = data.rows;
-        this.totalItems = data.total;
-      },
-      err => {
-        console.log(err);
-      }
-    );
-  }
+        this.postService.getList()
+            .take(1)
+            .subscribe(
+                (data: Post[]) => {
+                    this.dataSource.data = data;
+                },
+                err => {
+                    console.log(err);
+                }
+            );
+    }
 
 
-  public goToWrite(): void {
-    this.router.navigateByUrl('manage/write');
-  }
+    public goToWrite(): void {
+        this.router.navigateByUrl('manage/write');
+    }
 
-  public editPost(post): void {
-    this.router.navigateByUrl('manage/p/' + post.id + '/edit');
-  }
+    public editPost(post): void {
+        this.router.navigateByUrl('manage/p/' + post.id + '/edit');
+    }
 
-  public top(post: Post): void {
-    this.postService.edit(post).subscribe();
-  }
+    public top(post: Post): void {
+        this.postService.edit(post).subscribe();
+    }
 
-  public unTop(post: Post): void {
-    this.postService.edit(post).subscribe();
-  }
+    public unTop(post: Post): void {
+        this.postService.edit(post).subscribe();
+    }
 
-  public delPost(post: Post): void {
+    public delPost(post: Post): void {
 
-    this.postService.delete(post.id).subscribe(
-      data => {
-        const offset = (this.currentPage - 1) * this.itemsPerPage;
-        const rows = this.itemsPerPage;
-        const total = -1;
+        this.postService.delete(post.id).subscribe(
+            data => {
+                this.loadData();
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    }
 
-        this.loadData(offset, rows, total);
-      },
-      err => {
-        console.log(err);
-      }
-    );
-  }
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+    }
+
+    applyFilter(filterValue: string) {
+        console.log(filterValue);
+        filterValue = filterValue.trim(); // Remove whitespace
+        filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+        this.dataSource.filter = filterValue;
+    }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import {
     MatPaginatorModule,
     MatSidenavModule,
     MatSlideToggleModule,
+    MatTableModule,
     MatToolbarModule,
 } from '@angular/material';
 
@@ -46,6 +47,7 @@ import {
         MatPaginatorModule,
         MatSidenavModule,
         MatSlideToggleModule,
+        MatTableModule,
         MatToolbarModule,
     ],
     exports: [
@@ -63,6 +65,7 @@ import {
         MatPaginatorModule,
         MatSidenavModule,
         MatSlideToggleModule,
+        MatTableModule,
         MatToolbarModule,
         CustomDatePipe,
         EscapeHtmlPipe,
