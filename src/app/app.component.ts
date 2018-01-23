2263b061239dfc0d59b5ae6835053b1ddc2786a2
fuzhutech <fuzhutech@163.com>
2018-01-23 23:40:33 +0800

    研究angular-material主题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostBinding, Version, OnInit} from '@angular/core';
 import {OverlayContainer} from '@angular/cdk/overlay';
 
 @Component({
@@ -10,19 +10,26 @@ export class AppComponent {
 
     darkTheme = false;
 
+    @HostBinding('class') componentCssClass = 'myapp-light-theme';
+
     constructor(private overlayContainer: OverlayContainer) {
     }
 
     switchTheme(dark: boolean) {
         this.darkTheme = dark;
         if (dark) {
+            this.componentCssClass = 'myapp-dark-theme';
             /**
              * 由于某些组件（如菜单、选择、对话框等）在一个全局覆盖容器中，这些组件需要受主题CSS类选择器的影响,
              * 需要额外的步骤将全局样式类添加到全局覆盖容器中。
              */
+            this.overlayContainer.getContainerElement().classList.remove('myapp-light-theme');
             this.overlayContainer.getContainerElement().classList.add('myapp-dark-theme');
         } else {
+            this.componentCssClass = 'myapp-light-theme';
             this.overlayContainer.getContainerElement().classList.remove('myapp-dark-theme');
+            this.overlayContainer.getContainerElement().classList.add('myapp-light-theme');
         }
     }
+
 }
