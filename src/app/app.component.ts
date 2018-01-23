dba485c381d487b61bb1a4bfc6615d6d2d24d373
fuzhutech <fuzhutech@163.com>
2018-01-23 17:25:48 +0800

    自定义主题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,10 @@ export class AppComponent {
     switchTheme(dark: boolean) {
         this.darkTheme = dark;
         if (dark) {
+            /**
+             * 由于某些组件（如菜单、选择、对话框等）在一个全局覆盖容器中，这些组件需要受主题CSS类选择器的影响,
+             * 需要额外的步骤将全局样式类添加到全局覆盖容器中。
+             */
             this.overlayContainer.getContainerElement().classList.add('myapp-dark-theme');
         } else {
             this.overlayContainer.getContainerElement().classList.remove('myapp-dark-theme');
