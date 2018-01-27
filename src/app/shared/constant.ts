85e8baf5dc6ad8e731bcce7ef0b8c228af58a461
fuzhutech <fuzhutech@163.com>
2018-01-27 23:22:18 +0800

    使用angular-cli的--proxy-config功能

--- src/app/shared/constant.ts
+++ src/app/shared/constant.ts
@@ -1,2 +1,11 @@
-export const HOST_PATH = 'http://localhost';
-export const HOST_API_PATH = 'http://localhost/api';
+// 使用nginx反向代理开发
+// export const HOST_PATH = 'http://localhost';
+// export const HOST_API_PATH = 'http://localhost/api';
+
+// 使用 --proxy-config配置开发
+export const HOST_PATH = 'http://localhost:4200';
+export const HOST_API_PATH = 'http://localhost:4200/api';
+
+// 发布应用时配置
+// export const HOST_PATH = 'http://www.fuzhutech.com';
+// export const HOST_API_PATH = 'http://www.fuzhutech.com/api';
